363a4f4e5e8122840b0dfa1af647acbae22a61aa
marut97 <marut1997@gmail.com>
2021-10-25 22:47:22 +0530

    Adding logo and animation

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.css";
+import logo from "./../../assets/logo.svg";
 
 const Header = (props) => {
   return (
     <div className="headerBar">
-      <img className="logo" src="./../../assets/logo.svg" />
+      <img src={logo} alt="Logo" className="movieLogo" />
     </div>
   );
 };
