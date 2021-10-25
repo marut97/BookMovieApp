7de345cb40f239abf0b657a72febbf8ef8c05c5d
marut97 <marut1997@gmail.com>
2021-10-25 23:47:29 +0530

    Adding login, logout and book show buttons

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./Header.css";
 import logo from "./../../assets/logo.svg";
+import Button from "@material-ui/core/Button";
 
 const Header = (props) => {
   return (
     <div className="headerBar">
       <img src={logo} alt="Logo" className="movieLogo" />
+      <div className="container">
+      <Button variant="contained" color="primary" className="bookShowButton">
+          BOOK SHOW
+        </Button>
+        &nbsp;&nbsp;
+        <Button variant="contained" color="default" className="loginButton">
+          LOGIN
+        </Button>
+        &nbsp;&nbsp;
+        <Button variant="contained" color="default" className="logoutButton">
+          LOGOUT
+        </Button>
+      </div>
     </div>
   );
 };
