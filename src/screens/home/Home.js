1a437144dd86495c7285b4dd850f78aaefa4b3f4
marut97 <marut1997@gmail.com>
2021-10-27 00:29:02 +0530

    Upcoming movies implementation

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Home.css";
+import Header from "../../common/header/Header";
+import UpcomingMovies from "./UpcomingMovies";
 
 const Home = (props) => {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <Header></Header>
+      <UpcomingMovies />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/screens/home/UpcomingMovies.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { GridList, GridListTile, GridListTileBar } from "@material-ui/core";
+import "./Home.css";
+
+const UpcomingMovies = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const response = await fetch(
+        "http://localhost:8085/api/v1/movies?status=PUBLISHED"
+      );
+      const data = await response.json();
+      setMovies(data.movies);
+    };
+
+    load();
+  }, []);
+
+  return (
+    <div>
+      <div className="upcomingMoviesHeader">Upcoming Movies</div>
+      <GridList cols={6} style={{ flexWrap: "nowrap" }}>
+        {movies.map((movie) => (
+          <GridListTile className="movieTitle" key={movie.id}>
+            <img src={movie.poster_url} alt={movie.title} />
+            <GridListTileBar title={movie.title} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+};
+
+export default UpcomingMovies;
