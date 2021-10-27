b09aa89d5e420c6f527d40d9f733187723e72a6c
marut97 <marut1997@gmail.com>
2021-10-27 17:01:55 +0530

    Implementing rating

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -9,7 +9,7 @@ import {
 import { Link } from "react-router-dom";
 import "./Details.css";
 import YouTube from "react-youtube";
-import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import Ratings from "./Ratings";
 
 const Content = (props) => {
   // Declare state variables
@@ -46,78 +46,79 @@ const Content = (props) => {
   }, []);
 
   return (
-<div className="details">
-        <div className="leftSection">
-          <img src={movieData.poster_url} alt={movieData.title} className="poster"/>
-        </div>
-        <div className="centerSection">
-          <Typography variant="headline" component="h2">
-            {movieData.title}
-          </Typography>
-          <br />
-          <Typography>
-            <b>Genre:</b> {genreData}
-          </Typography>
-          <br />
-          <Typography>
-            <b>Duration:</b> {movieData.duration}
-          </Typography>
-          <br />
-          <Typography>
-            <b>Release Date:</b> {dateData}
-          </Typography>
-          <br />
-          <Typography>
-            <b>Rating:</b> {movieData.rating}
-          </Typography>
-          <br />
-          <Typography className="plot">
-            <b>Plot:</b> ( <a href={movieData.wiki_url}>Wiki Link</a>){" "}
-            {movieData.storyline}
-          </Typography>
-          <br />
-          <Typography className="trailer">
-            <b>Trailer:</b>
-          </Typography>
-          <br />
-          <YouTube videoId={ytData} className="player" />
-        </div>
-        <div className="rightSection">
-          <Typography>
-            <b>Rate this movie:</b>
-            <br />
-          </Typography>
-          <StarBorderOutlinedIcon className="hover-star" />
-          <StarBorderOutlinedIcon className="hover-star" />
-          <StarBorderOutlinedIcon className="hover-star" />
-          <StarBorderOutlinedIcon className="hover-star" />
-          <StarBorderOutlinedIcon className="hover-star" />
-          <Typography className="artist">
-            <b>Artists:</b>
-            <br />
-          </Typography>
-          <GridList cols={2} style={{margin:"2px"}}>
-            {artistData.map((artist) => (
-              <GridListTile key={artist.id}>
-                <img
-                  src={artist.profile_url}
-                  alt={`${artist.first_name} ${artist.last_name}`}
-                />
-                <GridListTileBar
-                  title={`${artist.first_name} ${artist.last_name}`}
-                />
-              </GridListTile>
-            ))}
-          </GridList>
-        </div>
+    <div className="details">
+      <div className="leftSection">
+        <img
+          src={movieData.poster_url}
+          alt={movieData.title}
+          className="poster"
+        />
       </div>
+      <div className="centerSection">
+        <Typography variant="headline" component="h2">
+          {movieData.title}
+        </Typography>
+        <br />
+        <Typography>
+          <b>Genre:</b> {genreData}
+        </Typography>
+        <br />
+        <Typography>
+          <b>Duration:</b> {movieData.duration}
+        </Typography>
+        <br />
+        <Typography>
+          <b>Release Date:</b> {dateData}
+        </Typography>
+        <br />
+        <Typography>
+          <b>Rating:</b> {movieData.rating}
+        </Typography>
+        <br />
+        <Typography className="plot">
+          <b>Plot:</b> ( <a href={movieData.wiki_url}>Wiki Link</a>){" "}
+          {movieData.storyline}
+        </Typography>
+        <br />
+        <Typography className="trailer">
+          <b>Trailer:</b>
+        </Typography>
+        <br />
+        <YouTube videoId={ytData} className="player" />
+      </div>
+      <div className="rightSection">
+        <Ratings />
+        <Typography className="artist">
+          <b>Artists:</b>
+          <br />
+        </Typography>
+        <GridList cols={2} style={{ margin: "2px" }}>
+          {artistData.map((artist) => (
+            <GridListTile key={artist.id}>
+              <img
+                src={artist.profile_url}
+                alt={`${artist.first_name} ${artist.last_name}`}
+              />
+              <GridListTileBar
+                title={`${artist.first_name} ${artist.last_name}`}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    </div>
   );
 };
 
 const Details = function (props) {
   return (
     <div>
-      <Header loggedIn={props.loggedIn}  loggedInChanged={props.loggedInChanged} BookShowVisible={true} id={props.match.params.id}/>
+      <Header
+        loggedIn={props.loggedIn}
+        loggedInChanged={props.loggedInChanged}
+        BookShowVisible={true}
+        id={props.match.params.id}
+      />
       <Link to={"/"} className="link">
         <Typography className="back">&#60; Back to Book Show</Typography>
       </Link>
---
+++ src/screens/details/Ratings.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Typography, IconButton } from "@material-ui/core";
+import "./Details.css";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+
+const RatingButton = function(props) {
+    return (
+        <IconButton
+        type="button"
+        key={props.index}
+        style={{backgroundColor: 'transparent', color:`${props.index <= props.rating ? "yellow" : props.index <= props.hover ? "#fefebe" : "black"}`}}
+        onClick={() => props.setRating(props.index)}
+        onMouseEnter={() => props.setHover(props.index)}
+        onMouseLeave={() => props.setHover(props.rating)}
+      >
+        <StarBorderOutlinedIcon />
+      </IconButton>
+    );
+}
+
+const Ratings = function () {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  return (
+    <div>
+      <Typography>
+        <b>Rate this movie:</b>
+        <br />
+      </Typography>
+
+
+      <RatingButton index={1} rating={rating} setRating={setRating} hover={hover} setHover={setHover}/>
+      <RatingButton index={2} rating={rating} setRating={setRating} hover={hover} setHover={setHover}/>
+      <RatingButton index={3} rating={rating} setRating={setRating} hover={hover} setHover={setHover}/>
+      <RatingButton index={4} rating={rating} setRating={setRating} hover={hover} setHover={setHover}/>
+      <RatingButton index={5} rating={rating} setRating={setRating} hover={hover} setHover={setHover}/>
+    </div>
+  );
+};
+
+export default Ratings;
